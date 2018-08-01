407d9befdf56ce8a761e6380a06c4cdaaa92cb33
wukunpdd <wukunpdd@163.com>
2018-08-01 17:12:51 +0800

    条件化的bean

--- conditional/src/main/java/cn/wukun/MagicExistsCondition.java
+++ conditional/src/main/java/cn/wukun/MagicExistsCondition.java
@@ -7,6 +7,12 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 
 public class MagicExistsCondition implements Condition {
 
+  /**
+   * 环境中是否包含magic属性作为判断条件，只有包含magic属性才会创建对应@Conditional注解下的bean
+   * @param context
+   * @param metadata
+   * @return
+   */
   @Override
   public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
     Environment env = context.getEnvironment();
