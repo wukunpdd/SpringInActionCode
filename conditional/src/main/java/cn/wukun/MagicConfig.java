d61cbc53944ec2c55fc529c33c05510338fdf848
wukunpdd <wukunpdd@163.com>
2018-08-01 17:14:43 +0800

    条件化的bean

--- conditional/src/main/java/cn/wukun/MagicConfig.java
+++ conditional/src/main/java/cn/wukun/MagicConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class MagicConfig {
 
   @Bean
-  @Conditional(MagicExistsCondition.class) //设置给@Conditional的类可以是任意实现了Condition接口的类型
+  @Conditional(MagicExistsCondition.class) //设置给@Conditional的类可以是任意实现了Condition接口的类型，只有当实现类中matches()中返回为true，才创建magicBean
   public MagicBean magicBean() {
     return new MagicBean();
   }
