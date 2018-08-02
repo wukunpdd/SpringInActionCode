40cd99ec5a2ad1c8ce3075ea995bfeb808aadd89
wukunpdd <wukunpdd@163.com>
2018-08-02 18:36:58 +0800

    面向切面的Spring

--- aspect/src/main/java/cn/wukun/TrackCounter.java
+++ aspect/src/main/java/cn/wukun/TrackCounter.java
@@ -1,5 +1,7 @@
 package cn.wukun;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -13,5 +15,19 @@ public class TrackCounter {
     public void beforeMethod( int i) {
         System.out.println("---------begins with " + i);
     }
+
+    @Around(value = "declareJoinPointExpression(i)", argNames = "point, i")
+    public Object Method(ProceedingJoinPoint point, int i) {
+        Object result = null;
+        try {
+            result = point.proceed();
+        } catch (Throwable e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("---------Around with " + i +"-"+result);
+        return 33;
+    }
+
 
 }
