e4f9167fc1040a2ca0628b7c14c274e85b6cfde1
wukunpdd <wukunpdd@163.com>
2018-08-08 15:46:02 +0800

    删除测试

--- test/src/main/java/cn/wukun/data/SpittleRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.wukun.data;
-
-import cn.wukun.domain.Spittle;
-
-import java.util.List;
-
-public interface SpittleRepository {
-    List<Spittle> findSpittleByUserName(String userName);
-}
