aa24059ece733f56aa662dd4f15e4cfdbc1d525d
wukunpdd <wukunpdd@163.com>
2018-08-24 17:18:38 +0800

    删除重建

--- cache/src/main/java/cn/wukun/zero/UserExtDao.java
+++
@@ -1,8 +0,0 @@
-package cn.wukun.zero;
-
-import cn.wukun.domain.UserExt;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserExtDao extends JpaRepository<UserExt, Integer> {
-    UserExt save(UserExt userExt);
-}
